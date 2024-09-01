e98e6c0678f51e972669a5c2007942b9f264f9dc
Verox001 <verox@cimeyclust.com>
2024-09-01 15:31:09 +0200

    Added property to display mining status

--- src/ISRUApi/Modules/Data_Mining.cs
+++ src/ISRUApi/Modules/Data_Mining.cs
@@ -1,4 +1,5 @@
-﻿using KSP.Sim;
+﻿using System.ComponentModel;
+using KSP.Sim;
 using KSP.Sim.Definitions;
 using Newtonsoft.Json;
 
@@ -10,10 +11,28 @@ public class Data_Mining: ModuleData
     public override Type ModuleType => typeof(Module_Mining);
 
     [KSPState(CopyToSymmetrySet = true)]
-    [PAMDisplayControl(ExcludeFromContext = true)]
+    [PAMDisplayControl(ExcludeFromContext = true, SortIndex = 2)]
     [LocalizedField("ISRUApi/Modules/Data_Mining/deployed")]
     public ModuleProperty<bool> deployed = new ModuleProperty<bool>(false);
 
+    [LocalizedField("ISRUApi/Modules/Data_Mining/status")]
+    [PAMDisplayControl(SortIndex = 1)]
+    public ModuleProperty<MiningState> controlStatus = new ModuleProperty<MiningState>(MiningState.Stopped, true);
+
     [JsonIgnore]
     public PartComponentModule_Mining PartComponentModule;
+
+    protected override void InitProperties()
+    {
+        base.InitProperties();
+
+        controlStatus.SetValue(deployed.GetValue() ? MiningState.Mining : MiningState.Stopped);
+    }
+}
+
+public enum MiningState : byte
+{
+    None = 0,
+    [Description("ISRUApi/Modules/Data_Mining/status/Mining")] Mining = 1,
+    [Description("ISRUApi/Modules/Data_Mining/status/Stopped")] Stopped = 2,
 }
